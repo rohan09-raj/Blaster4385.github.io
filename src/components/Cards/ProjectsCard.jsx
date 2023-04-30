afe7e866624c3e8b9130ffb783d3e968dafbb235
Blaster4385 <venkateshchaturvedi12@gmail.com>
2023-04-30 15:32:06 +0530

    Optimise code and add icon to buttons

--- src/components/Cards/ProjectsCard.jsx
+++ src/components/Cards/ProjectsCard.jsx
@@ -4,6 +4,30 @@ import { useState } from 'react'
 
 const ProjectsCard = ({ open, setOpen }) => {
     const [maximise, setMaximise] = useState(false)
+
+    const data = [
+        {
+            title: 'PixelBlaster-OS',
+            description: 'PixelBlaster-OS is a customised fork of the Android Open Source Project (AOSP)',
+            link: 'https://github.com/PixelBlaster-OS'
+        },
+        {
+            title: 'IllusionX Kernel',
+            description: 'IllusionX is a customised Linux kernel for the OnePlus 8T/9R',
+            link: 'https://github.com/Blaster4385/kernel_oneplus_sm8250'
+        },
+        {
+            title: 'Device Tree for OnePlus 8T/9R',
+            description: 'This is a device tree for compiling the Android Open Source Project (AOSP) for the OnePlus 8T/9R',
+            link: 'https://github.com/PixelBlaster-Devices/device_oneplus_opkona'
+        },
+        {
+            title: 'Device Tree for Realme 3 Pro',
+            description: 'This is a device tree for compiling the Android Open Source Project (AOSP) for the Realme 3 Pro',
+            link: 'https://github.com/PixelBlaster-Devices/device_realme_RMX1851'
+        }
+    ]
+
     return (
         <div className={maximise ? open ? `${styles.card} ${styles.card__open} ${styles.card__maximise}` : `${styles.card} ${styles.card__maximise}` : open ? `${styles.card} ${styles.card__open}` : styles.card}>
             <button className={styles.card__close__button} onClick={() => setOpen(!open)}>&times;</button>
@@ -11,26 +35,13 @@ const ProjectsCard = ({ open, setOpen }) => {
             <div className={styles.card__title}>
                 <h1>Projects</h1>
             </div>
+            {data.map((item) => (
             <div className={styles.card__content}>
-                <h2>PixelBlaster-OS</h2>
-                <p>PixelBlaster-OS is a customised fork of the Android Open Source Project (AOSP)</p>
-                <button className={styles.card__button}><a href="https://github.com/PixelBlaster-OS">View on Github</a></button>
+                <h2>{item.title}</h2>
+                <p>{item.description}</p>
+                <button className={styles.card__button}><img className={styles.card__button__icon} src='/assets/images/git.png' /><a href={item.link}>View on Github</a></button>
             </div>
-            <div className={styles.card__content}>
-                <h2>IllusionX Kernel</h2>
-                <p>IllusionX is a customised Linux kernel for the OnePlus 8T/9R</p>
-                <button className={styles.card__button}><a href="https://github.com/Blaster4385/kernel_oneplus_sm8250">View on Github</a></button>
-            </div>
-            <div className={styles.card__content}>
-                <h2>Device Tree for OnePlus 8T/9R</h2>
-                <p>This is a device tree for compiling the Android Open Source Project (AOSP) for the OnePlus 8T/9R</p>
-                <button className={styles.card__button}><a href="https://github.com/PixelBlaster-Devices/device_oneplus_opkona">View on Github</a></button>
-            </div>
-            <div className={styles.card__content}>
-                <h2>Device Tree for Realme 3 Pro</h2>
-                <p>This is a device tree for compiling the Android Open Source Project (AOSP) for the Realme 3 Pro</p>
-                <button className={styles.card__button}><a href="https://github.com/PixelBlaster-Devices/device_realme_RMX1851">View on Github</a></button>
-            </div>
+            ))}
         </div>
     )
 }
